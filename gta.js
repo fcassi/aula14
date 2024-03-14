51690619dd2fbaeafc1f528dae13c1d04ac89f15
FELIPE ROBERTO CASSIANO <felipe.rcassiano@senacsp.edu.br>
2024-03-14 10:57:27 -0300

    atividade

---
+++ gta.js
@@ -0,0 +1,10 @@
+let modal = document.querySelector(".modal")
+
+function abrir() {
+    if (modal.style.display == "none") {
+        modal.style.display = "block"
+    } else {
+        modal.style.display = "none"
+    }
+}
+
